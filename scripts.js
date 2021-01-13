64d14f6292efc73df1cd7adaaa4cdff4d458c8fa
Caiden Tousaw <60043341+CaidenTousaw@users.noreply.github.com>
2021-01-12 20:15:12 -0800

    Website Draft
    day one of working on site, this is basically it except for like more in "About."

--- scripts.js
+++ scripts.js
@@ -1,3 +1,75 @@
-function Main() {
-  
+$(document).ready(function(){
+  $(".youtube").hover(function(){
+    $(".header-logo").addClass("red_border");
+  }, function () {
+    $(".header-logo").removeClass("red_border");
+  });
+});
+
+$(document).ready(function(){
+  $(".twitch").hover(function(){
+    $(".header-logo").addClass("twitch_border");
+  }, function () {
+    $(".header-logo").removeClass("twitch_border");
+  });
+});
+
+$(document).ready(function(){
+  $(".steam").hover(function(){
+    $(".header-logo").addClass("steam_border");
+  }, function () {
+    $(".header-logo").removeClass("steam_border");
+  });
+});
+
+$(document).ready(function(){
+  $(".twitter").hover(function(){
+    $(".header-logo").addClass("twitter_border");
+  }, function () {
+    $(".header-logo").removeClass("twitter_border");
+  });
+});
+
+$(document).ready(function(){
+  $(".github").hover(function(){
+    $(".header-logo").addClass("github_border");
+  }, function () {
+    $(".header-logo").removeClass("github_border");
+  });
+});
+
+$(document).ready(function(){
+  $(".spotify").hover(function(){
+    $(".header-logo").addClass("spotify_border");
+  }, function () {
+    $(".header-logo").removeClass("spotify_border");
+  });
+});
+
+$(document).ready(function(){
+  $(".srdc").hover(function(){
+    $(".header-logo").addClass("srdc_border");
+  }, function () {
+    $(".header-logo").removeClass("srdc_border");
+  });
+});
+$(document).ready(function(){
+  $(".instagram").hover(function(){
+    $(".header-logo").addClass("insta_border");
+  }, function () {
+    $(".header-logo").removeClass("insta_border");
+  });
+});
+
+$(document).ready(function(){
+  $(".snap").hover(function(){
+    $(".header-logo").addClass("snap_border");
+  }, function () {
+    $(".header-logo").removeClass("snap_border");
+  });
+});
+
+
+function DontNeed() {
+  window.confirm("You don't need to! Are you sure?")
 }
